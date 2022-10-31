ecf4e3cc8118212134848a030be1efa01837dce5
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-10-31 18:49:30 -0300

    first commit

---
+++ src/main/java/github/pedrohpalmeida/sistemaDespesas/App.java
@@ -0,0 +1,13 @@
+package github.pedrohpalmeida.sistemaDespesas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/github/pedrohpalmeida/sistemaDespesas/AppTest.java
@@ -0,0 +1,20 @@
+package github.pedrohpalmeida.sistemaDespesas;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
