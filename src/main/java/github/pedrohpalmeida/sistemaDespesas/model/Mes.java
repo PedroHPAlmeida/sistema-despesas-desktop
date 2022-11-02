b14b86d0e44470527647571b9f11b2e6141e4986
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-11-02 14:28:18 -0300

    feat: adicionando camada de persistencia da entidade

---
+++ src/main/java/github/pedrohpalmeida/sistemaDespesas/model/Mes.java
@@ -0,0 +1,23 @@
+package github.pedrohpalmeida.sistemaDespesas.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+@Table(name = "tb_meses")
+@Entity
+public class Mes {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    public Mes(String nome) {
+        this.nome = nome;
+    }
+}
